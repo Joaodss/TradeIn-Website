3ade5c1b0d9d463ff7b2b973921ff32e0f212976
João Afonso Silva <joaoafonsodss@gmail.com>
2022-02-24 15:20:23 +0000

    add temporary integration test

---
+++ src/test/java/com/joaodss/tradeinwebsite/TemporaryIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.joaodss.tradeinwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TemporaryIntegrationTest {
+
+    @Test
+    void temporaryEmpty() {
+    }
+
+}
