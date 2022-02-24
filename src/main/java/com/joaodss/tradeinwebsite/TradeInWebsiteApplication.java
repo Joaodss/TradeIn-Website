8833a2dde0109d397af946cc341066c15453825a
João Afonso Silva <joaoafonsodss@gmail.com>
2022-02-24 15:03:24 +0000

    Initialize empty project

---
+++ src/main/java/com/joaodss/tradeinwebsite/TradeInWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.joaodss.tradeinwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeInWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeInWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joaodss/tradeinwebsite/TradeInWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodss.tradeinwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeInWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
