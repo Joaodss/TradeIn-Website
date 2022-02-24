1e53529e86e52c3f9726e3a83a358827cc926fc4
João Afonso Silva <joaoafonsodss@gmail.com>
2022-02-24 15:43:52 +0000

    change testing profile

--- src/test/java/com/joaodss/tradeinwebsite/TemporaryIntegrationTest.java
+++ src/test/java/com/joaodss/tradeinwebsite/TemporaryIntegrationTest.java
@@ -2,8 +2,10 @@ package com.joaodss.tradeinwebsite;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class TemporaryIntegrationTest {
 
     @Test
