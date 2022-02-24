8efd59f576342b3d388b79012a65052df8764933
João Afonso Silva <joaoafonsodss@gmail.com>
2022-02-24 23:29:26 +0000

    embed bagphotos

--- src/main/java/com/joaodss/tradeinwebsite/dao/Bag.java
+++ src/main/java/com/joaodss/tradeinwebsite/dao/Bag.java
@@ -1,5 +1,6 @@
 package com.joaodss.tradeinwebsite.dao;
 
+import com.joaodss.tradeinwebsite.datatype.BagPhotos;
 import com.joaodss.tradeinwebsite.enums.BagExtra;
 import com.joaodss.tradeinwebsite.enums.BagSize;
 import lombok.*;
@@ -30,5 +31,23 @@ public class Bag extends Product {
     @CollectionTable(name = "bag_extra")
     @Column(name = "extra")
     private Set<BagExtra> bagExtras;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "externalFrontURL", column = @Column(name = "external_front_photo_URL")),
+            @AttributeOverride(name = "externalBackURL", column = @Column(name = "external_back_photo_URL")),
+            @AttributeOverride(name = "externalBottomURL", column = @Column(name = "external_bottom_photo_URL")),
+            @AttributeOverride(name = "externalLogoURL", column = @Column(name = "external_logo_photo_URL")),
+            @AttributeOverride(name = "externalSerialURL", column = @Column(name = "external_serial_photo_URL")),
+            @AttributeOverride(name = "externalZipperURL", column = @Column(name = "external_zipper_photo_URL")),
+            @AttributeOverride(name = "externalHardwareURL", column = @Column(name = "external_hardware_photo_URL")),
+            @AttributeOverride(name = "internalInsideURL", column = @Column(name = "internal_inside_photo_URL")),
+            @AttributeOverride(name = "internalBottomURL", column = @Column(name = "internal_bottom_photo_URL")),
+            @AttributeOverride(name = "internalLogoURL", column = @Column(name = "internal_logo_photo_URL")),
+            @AttributeOverride(name = "internalSerialURL", column = @Column(name = "internal_serial_photo_URL")),
+            @AttributeOverride(name = "internalZipperURL", column = @Column(name = "internal_zipper_photo_URL")),
+            @AttributeOverride(name = "internalHardwareURL", column = @Column(name = "internal_hardware_photo_URL")),
+    })
+    private BagPhotos bagPhotos;
 
 }
