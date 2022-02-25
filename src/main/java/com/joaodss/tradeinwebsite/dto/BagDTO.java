28f20056fd115a5b72cfb108301a353c861833af
João Afonso Silva <joaoafonsodss@gmail.com>
2022-02-25 18:06:22 +0000

    comment photos

--- src/main/java/com/joaodss/tradeinwebsite/dto/BagDTO.java
+++ src/main/java/com/joaodss/tradeinwebsite/dto/BagDTO.java
@@ -14,17 +14,17 @@ public class BagDTO {
     private String size;
     private Set<String> extras;
 
-    private String externalFrontURL;
-    private String externalBackURL;
-    private String externalBottomURL;
-    private String externalLogoURL;
-    private String externalSerialURL;
-    private String externalZipperURL;
-    private String externalHardwareURL;
-    private String internalInsideURL;
-    private String internalBottomURL;
-    private String internalLogoURL;
-    private String internalSerialURL;
-    private String internalZipperURL;
-    private String internalHardwareURL;
+//    private String externalFrontURL;
+//    private String externalBackURL;
+//    private String externalBottomURL;
+//    private String externalLogoURL;
+//    private String externalSerialURL;
+//    private String externalZipperURL;
+//    private String externalHardwareURL;
+//    private String internalInsideURL;
+//    private String internalBottomURL;
+//    private String internalLogoURL;
+//    private String internalSerialURL;
+//    private String internalZipperURL;
+//    private String internalHardwareURL;
 }
--- src/main/java/com/joaodss/tradeinwebsite/dto/ShoesDTO.java
+++ src/main/java/com/joaodss/tradeinwebsite/dto/ShoesDTO.java
@@ -11,15 +11,15 @@ import lombok.*;
 public class ShoesDTO {
     private Short size;
 
-    private String externalTopURL;
-    private String externalLeftSideURL;
-    private String externalRightSideURL;
-    private String externalBottomURL;
-    private String externalLogoURL;
-    private String externalSerialURL;
-    private String externalZipperURL;
-    private String externalHardwareURL;
-    private String internalInsideURL;
-    private String internalLogoURL;
-    private String internalSerialURL;
+//    private String externalTopURL;
+//    private String externalLeftSideURL;
+//    private String externalRightSideURL;
+//    private String externalBottomURL;
+//    private String externalLogoURL;
+//    private String externalSerialURL;
+//    private String externalZipperURL;
+//    private String externalHardwareURL;
+//    private String internalInsideURL;
+//    private String internalLogoURL;
+//    private String internalSerialURL;
 }
