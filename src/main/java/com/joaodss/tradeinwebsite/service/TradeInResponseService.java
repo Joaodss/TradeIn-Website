0447ce20bd4140235867df25450429946cf194f0
João Afonso Silva <joaoafonsodss@gmail.com>
2022-03-02 00:05:38 +0000

    add missing service abstractions

--- src/main/java/com/joaodss/tradeinwebsite/service/TradeInResponseService.java
+++ src/main/java/com/joaodss/tradeinwebsite/service/TradeInResponseService.java
@@ -1,5 +1,7 @@
 package com.joaodss.tradeinwebsite.service;
 
+import com.joaodss.tradeinwebsite.dao.TradeInRequest;
+import com.joaodss.tradeinwebsite.dto.ResponseProductDTO;
 import com.joaodss.tradeinwebsite.dto.ResponseTradeInRequestDTO;
 import com.joaodss.tradeinwebsite.dto.TradeInRequestDTO;
 
@@ -15,6 +17,18 @@ public interface TradeInResponseService {
 
     ResponseTradeInRequestDTO create(TradeInRequestDTO tradeInRequestDTO);
 
+    TradeInRequest saveToDatabase(TradeInRequest tradeInRequest);
+
+    void exportToGoogleSheets(ResponseTradeInRequestDTO tradeInRequest);
+
     ResponseTradeInRequestDTO delete(long id);
+
+    void deleteFromDatabase(long id);
+
+    void deleteFromGoogleSheets(long id);
+
+    void deleteProductsFromGoogleDrive(List<ResponseProductDTO> products);
+
+    void deleteProductPhotos(String filesPath);
 
 }
